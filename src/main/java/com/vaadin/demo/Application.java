63281c3bac1bb8f169153c913f471a0641ca7643
Sebastian Kühnau <seb@vaadin.com>
2025-07-15 18:45:35 +0200

    initial commit

--- src/main/java/com/vaadin/demo/Application.java
+++ src/main/java/com/vaadin/demo/Application.java
@@ -1,7 +1,10 @@
 package com.vaadin.demo;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.LoadingIndicatorConfiguration;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,9 +19,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @PageTitle("Loading Indicator Demo")
 @Theme(value = "loading-indicator-demo")
-public class Application implements AppShellConfigurator {
+public class Application implements AppShellConfigurator, VaadinServiceInitListener {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void serviceInit(ServiceInitEvent serviceInitEvent) {
+        serviceInitEvent.getSource().addUIInitListener(uiInitEvent -> {
+            LoadingIndicatorConfiguration conf = uiInitEvent.getUI().getLoadingIndicatorConfiguration();
+
+            // disable default theme -> loading indicator isn't shown
+            conf.setApplyDefaultTheme(false);
+        });
     }
 }
--- src/main/java/com/vaadin/demo/views/demoview/DemoViewView.java
+++ src/main/java/com/vaadin/demo/views/demoview/DemoViewView.java
@@ -3,6 +3,7 @@ package com.vaadin.demo.views.demoview;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -21,6 +22,17 @@ public class DemoViewView extends Composite<VerticalLayout> {
         buttonPrimary.setText("Do Heavy Work");
         buttonPrimary.setWidth("min-content");
         buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonPrimary.addClickListener(event -> heavyWork(5000));
         getContent().add(buttonPrimary);
+    }
+
+    private void heavyWork(int milliseconds) {
+
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // sauber abbrechen
+        }
+        Notification.show("Heavy Work is done!");
     }
 }
